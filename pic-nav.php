b98571e8dff90fe90a5a1743d13d27ee7446656d
fengdaxian <feng1222@hnu.edu.cn>
2023-01-31 11:20:07 +0800

    增加图片放大功能

--- pic-nav.php
+++ pic-nav.php
@@ -13,9 +13,9 @@ $this->need('header.php'); ?>
 $options = Typecho_Widget::widget('Widget_Options');
 $pri_thumbs = explode("|", $options->bcool_cover); /*获取文章封面*/
 $pub_thumbs = explode("|", $options->public_bcool_cover);
-if($this->user->hasLogin()){
+if ($this->user->hasLogin()) {
     $thumbs = array_merge($pri_thumbs, $pub_thumbs);
-}else{
+} else {
     $thumbs = $pub_thumbs;
 }
 $num = count($thumbs);
@@ -37,12 +37,16 @@ if ($end >= $num) {
 </script>
 <div class="pageNav">
     <div class="centerbox">
-        <div class="discription">共计<?php echo $num; ?>张图片,当前</div>
+        <div class="discription">共计
+            <?php echo $num; ?>张图片,当前
+        </div>
         <form method="post">
             <input type="number" name="pageNum" id="page" min="1" max="<?php echo ceil($num / 100); ?>"
                 placeholder="<?php echo $pageNum; ?>" required="required" onmousewheel="return false;" />
 
-            <button type="submit" id="submit">/<?php echo ceil($num / 100); ?>页</button>
+            <button type="submit" id="submit">/
+                <?php echo ceil($num / 100); ?>页
+            </button>
         </form>
     </div>
 </div>
@@ -92,7 +96,7 @@ if ($end >= $num) {
 
     <style>
         .over {
-            background-color: rgba(0, 0, 0, 0.88);
+            background-color: rgba(255, 255, 255, 0.88);
             opacity: 1;
             filter: alpha(opacity=100);
             position: fixed;
@@ -110,7 +114,46 @@ if ($end >= $num) {
             position: absolute;
             z-index: 11;
             width: auto;
-            height: 99%;
+            height: 100%;
+        }
+
+        .leftarrow,
+        .rightarrow {
+            position: fixed;
+            width: 40px;
+            height: 30px;
+            background-color: rgba(0, 0, 0, 0.8);
+            border-radius: 20px;
+            text-align: center;
+            line-height: 30px;
+            font-size: 20px;
+            color: #fff;
+            z-index: 12;
+            top: 50%;
+            user-select: none;
+        }
+
+        .leftarrow {
+            left: 5%;
+        }
+
+        .rightarrow {
+            right: 5%;
+        }
+
+        .close {
+            position: fixed;
+            width: 40px;
+            height: 30px;
+            text-align: center;
+            line-height: 30px;
+            user-select: none;
+            border-radius: 15px;
+            top: 10%;
+            right: 5%;
+            background-color: rgba(0, 0, 0, 0.8);
+            z-index: 12;
+            color: #fff;
         }
     </style>
     <script>
@@ -120,10 +163,13 @@ if ($end >= $num) {
             for (var i = 0; i < imgs.length; i++) {
                 imgs[i].addEventListener('click', expandPhoto);
                 imgs[i].addEventListener('keydown', expandPhoto);
+                imgs[i].getElementsByTagName('img')[0].setAttribute("index", i);
             }
         }
         function expandPhoto() {
-        //  console.log('点击了');
+            // console.log('点击了');
+            var imgs = document.getElementsByClassName("content");
+            var maxlen = imgs.length;
             var overlay = document.createElement("div");
             overlay.setAttribute("id", "over");
             overlay.setAttribute("class", "over");
@@ -132,14 +178,104 @@ if ($end >= $num) {
             var img = document.createElement("img");
             img.setAttribute("id", "expand");
             img.setAttribute("class", "overimg");
-            img.src = this.getElementsByTagName('img')[0].getAttribute("src");
+            // img.setAttribute("src","https://www.fengdaxian.x…r/assets/img/loading.gif")
+            var tureimg = this.getElementsByTagName('img')[0];
+
+            img.src = tureimg.getAttribute("src");
             overlay.appendChild(img);
-            overlay.onclick = restore;
+
+            // function loadImage(url, callback) {
+            //     var img = new Image();
+            //     img.src = url;
+            //     img.onload = function () { //图片下载完毕时异步调用callback函数。 
+            //         callback.call(img); // 将callback函数this指针切换为img。 
+            //     };
+            // }
+
+            var flag = 1;
+            img.ondblclick=function(e){
+                console.log(e);
+                var x = window.innerWidth/2-e.clientX;
+                var y = window.innerHeight/2-e.clientY;
+                if (flag==1){
+                    img.style.transform="scale(2) translateX("+x+"px)"
+                    img.style.top = 2*y+"px";
+                    // img.style.height=window.innerHeight*2+"px";
+                    flag=0;
+                }else{
+                    img.style.transform="";
+                    img.style.top="0px";
+                    img.style.height="100%";
+                    flag=1;
+                }
+            }
+
+            var left = document.createElement("div");
+            left.setAttribute("id", "leftarrow");
+            left.setAttribute("class", "leftarrow");
+            left.innerHTML = "<<"
+            document.body.appendChild(left);
+            var index = tureimg.getAttribute('index');
+            left.onclick = function () {
+                // console.log(index);
+                var imgs = document.getElementsByClassName("content");
+                // console.log(tureimg.getAttribute('index'));
+                if (index > 0) {
+                    --index;
+                    img.src = "https://www.fengdaxian.xyz/usr/themes/gleaner/assets/img/picload.gif";
+                    var tempimg = new Image();
+                    tempimg.src = imgs[index].getElementsByTagName('img')[0].getAttribute('data-src');
+                    console.log(tempimg);
+                    tempimg.onload = function () { //图片下载完毕时异步调用callback函数。 
+                        img.setAttribute("src", imgs[index].getElementsByTagName('img')[0].getAttribute('data-src'));
+                    };
+                    // img.src = imgs[index].getElementsByTagName('img')[0].getAttribute('data-src');
+                } else {
+                    alert("到顶了，别点了");
+                }
+            };
+            var right = document.createElement("div");
+            right.setAttribute("id", "rightarrow");
+            right.setAttribute("class", "rightarrow");
+            right.innerHTML = ">>"
+            document.body.appendChild(right);
+            right.onclick = function () {
+                var imgs = document.getElementsByClassName("content");
+                // console.log(tureimg.getAttribute('index'));
+                if (index < maxlen - 1) {
+                    ++index;
+                    img.src = "https://www.fengdaxian.xyz/usr/themes/gleaner/assets/img/picload.gif";
+                    var tempimg = new Image();
+                    tempimg.src = imgs[index].getElementsByTagName('img')[0].getAttribute('data-src');
+                    console.log(tempimg);
+                    tempimg.onload = function () { //图片下载完毕时异步调用callback函数。 
+                        img.setAttribute("src", imgs[index].getElementsByTagName('img')[0].getAttribute('data-src'));
+                    };
+                    
+                    // img.src = imgs[index].getElementsByTagName('img')[0].getAttribute('data-src');
+                } else {
+                    alert("到底了，别点了");
+                }
+            };
+
+            var close = document.createElement("div");
+            close.setAttribute("class", "close");
+            close.setAttribute("id", "close");
+            close.innerHTML = "X";
+            document.body.appendChild(close);
+            close.onclick = restore;
+            // overlay.onclick = restore;
+
         }
         function restore() {
             document.body.removeChild(document.getElementById("over"));
+            document.body.removeChild(document.getElementById("leftarrow"));
+            document.body.removeChild(document.getElementById("rightarrow"));
+            document.body.removeChild(document.getElementById("close"));
         }
-        window.onload = addExpand;
+        // window.onload = addExpand;
+        // window.addEventListener('load', addExpand);
+        addExpand();
     </script>
 
     <script>
@@ -163,16 +299,16 @@ if ($end >= $num) {
     <script>
         $(document).ready(function () {
             $("#prevPage").click(function () {
-                if (<?php echo $pageNum ?> > 1){
-                post("", { "pageNum":<?php echo $pageNum - 1; ?>})
+                if (<?php echo $pageNum ?> > 1) {
+                    post("", { "pageNum":<?php echo $pageNum - 1; ?>})
                 }
             });
         });
         $(document).ready(function () {
 
             $("#nextPage").click(function () {
-                if (<?php echo $pageNum ?> < <?php echo ceil($num / 100) ?>){
-                post("", { "pageNum":<?php echo $pageNum + 1; ?>})
+                if (<?php echo $pageNum ?> < <?php echo ceil($num / 100) ?>) {
+                    post("", { "pageNum":<?php echo $pageNum + 1; ?>})
                 }
             });
         });
@@ -184,7 +320,10 @@ if ($end >= $num) {
             <div class="pages-controls-item-wrapper">
                 <div class="control-direction"><i class="fa-solid fa-arrow-left fa-lg"></i> 上一页</div>
                 <h3 class="post-title">
-                    <?php if ($pageNum == 1): ?>没有了~<?php else: ?>第<?php echo $pageNum - 1; ?>页<?php endif; ?>
+                    <?php if ($pageNum == 1): ?>没有了~
+                    <?php else: ?>第
+                        <?php echo $pageNum - 1; ?>页
+                    <?php endif; ?>
                 </h3>
             </div>
         </div>
@@ -193,7 +332,10 @@ if ($end >= $num) {
             <div class="pages-controls-item-wrapper">
                 <div class="control-direction">下一页 <i class="fa-solid fa-arrow-right fa-lg"></i></div>
                 <h3 class="post-title">
-                    <?php if ($pageNum == ceil($num / 100)): ?>没有了~<?php else: ?>第<?php echo $pageNum + 1; ?>页<?php endif; ?>
+                    <?php if ($pageNum == ceil($num / 100)): ?>没有了~
+                    <?php else: ?>第
+                        <?php echo $pageNum + 1; ?>页
+                    <?php endif; ?>
                 </h3>
             </div>
         </div>
